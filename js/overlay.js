8b186b09f0595910f2f31f312726e6d0547b9cd9
Bosun Olanrewaju <bosunolanrewaju@gmail.com>
2014-09-13 18:01:17 +0100

    Initial commit

---
+++ js/overlay.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+	$("#gallery li a").click(function(evt){
+		evt.preventDefault();
+		var imglink = $(this).attr("href");
+		var url = $(this).children("img").attr("alt");
+		$("#overlay").fadeIn();
+		var content = '<img src="'+ imglink + '" alt="">';
+			content += '<button type="button" formaction="http://bosunolanrewaju.github.io/' + url + '" class="btn btn-default">View Site</button>';
+		$("#overlay-img").html(content);
+		$("#overlay-img").fadeIn();
+	});
+
+	$(document).on("click", "button", function(){
+		var url = $(this).attr("formaction");
+		window.open(url);
+	});
+
+	$("#overlay").not("button").click(function() {
+		$("#overlay-img").fadeOut();
+		$(this).fadeOut();
+	})
+});
